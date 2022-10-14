9c83a2875c12ec4bc8eeaed70f1b60734de8187f
Ryan Joseph <ryankjoseph@gmail.com>
2022-10-13 21:56:55 -0400

    Change app to server and update db references.

--- config/database.js
+++ config/database.js
@@ -3,5 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/shows', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  // useCreateIndex: true (no longer supported)
+});
+
+const db = mongoose.connection;
+
+db.on('connected', function(){
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
